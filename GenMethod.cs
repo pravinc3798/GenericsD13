be99a1c702db736416c58958bbda88b97dd4e0c8
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-22 19:37:05 +0530

    Added GenMethod class

---
+++ GenMethod.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericsD13
+{
+    public class GenMethod
+    {
+       public static T FindMax<T>(T[] inputArray) where T : IComparable
+        {
+            T max = inputArray[0];
+
+            if (max.GetType() == typeof(string))
+            {
+                int length = max.ToString().Length;
+                foreach (T t in inputArray)
+                    max = (t.ToString().Length.CompareTo(length) > 0) ? t : max;
+                return max;
+            }
+
+            foreach (T t in inputArray)
+                max = (t.CompareTo(max) > 0) ? t : max;
+            return max;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GenericsD13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n ---- Using Specific Data Types ---- \n");
+
+            Console.WriteLine("\n >>>> Greatest Input : {0}", MaxInterger.Find());
+            Console.WriteLine("\n >>>> Greatest Input : {0}", MaxFloat.Find());
+            Console.WriteLine("\n >>>> Greatest Input : {0}", MaxString.Find());
+
+            Console.WriteLine("\n ---- Using Generic Method ---- \n");
+
+            Console.WriteLine("Enter 3 Integers \n"); var xyz = new int[3]; for (int i = 0; i < xyz.Length; i++) { Console.Write(" Provide input : "); xyz[i] = Convert.ToInt32(Console.ReadLine()); }
+            Console.WriteLine("\n >>>> Greatest Input : {0}", GenMethod.FindMax(xyz));
+
+            Console.WriteLine("Enter 3 Floats \n"); var abc = new float[3]; for (int i = 0; i < xyz.Length; i++) { Console.Write(" Provide input : "); abc[i] = float.Parse(Console.ReadLine()); }
+            Console.WriteLine("\n >>>> Greatest Input : {0}", GenMethod.FindMax(abc));
+
+            Console.WriteLine("Enter 3 Strings \n"); var def = new string[3]; for (int i = 0; i < xyz.Length; i++) { Console.Write(" Provide input : "); def[i] = Console.ReadLine(); }
+            Console.WriteLine("\n >>>> Greatest Input : {0}", GenMethod.FindMax(def));
+        }
+    }
+}
