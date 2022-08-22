d7b98ed2b0d1d13c2dca1b88c5257aeeafc62437
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-22 19:37:18 +0530

    Added MaxFloat class

---
+++ MaxFloat.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericsD13
+{
+    public class MaxFloat
+    {
+        public static float Find()
+        {
+            var size = 3;
+            var strings = new string[] { "st", "nd", "rd", "th" };
+            var list = new float[size];
+
+            Console.WriteLine("\n Enter {0} Floats \n", size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Enter {0}{1} Number : ", i + 1, (i < 3) ? strings[i] : strings[3]);
+                list[i] = float.Parse(Console.ReadLine());
+            }
+
+            var max = 0f;
+            foreach (var num in list)
+                max = (num > max) ? num : max;
+
+            return max;
+        }
+    }
+}
